94a8360c644dc5fed40da34fdf96b936cc9b72dd
dumamoiseev <duma.moiseev@gmail.com>
2023-08-28 11:20:42 +0200

    Update CreateOrderTests.java
    123

--- src/test/java/com/example/CreateOrderTests.java
+++ src/test/java/com/example/CreateOrderTests.java
@@ -42,7 +42,7 @@ public class CreateOrderTests {
                 .assertThat()
                 .statusCode(200)
                 .and()
-                .body("success", equalTo(true))
+                .body("success", equalTo(true2))
                 .body("order.number", notNullValue());
     }
 
