a13f06b2cb27e587ec3ae69a5c24d2529de4ea69
duma.electronik <duma.electronik@TeamCity>
2023-09-12 14:51:52 +0000

    TeamCity change in 'COPY_COPY_COPY' project: Connection removed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -51,15 +51,6 @@ project {
             privateKey = "credentialsJSON:4f1a5ccd-ca9e-49ae-a96a-175ea8790c81"
             ownerUrl = "https://github.com/username"
         }
-        githubAppConnection {
-            id = "PROJECT_EXT_9"
-            displayName = "added"
-            appId = "123"
-            clientId = "123"
-            clientSecret = "credentialsJSON:03c11ea2-e79b-4508-bb23-484668345ccd"
-            privateKey = "credentialsJSON:4f1a5ccd-ca9e-49ae-a96a-175ea8790c81"
-            ownerUrl = "https://github.com/username"
-        }
     }
 }
 
